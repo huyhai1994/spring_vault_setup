927063dd7cab3a210ccd1069a24b7dfd653cc17c
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-09-29 12:10:15 +0700

    first commit

---
+++ src/test/java/spring/learning/spring_vault/SpringVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.learning.spring_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
