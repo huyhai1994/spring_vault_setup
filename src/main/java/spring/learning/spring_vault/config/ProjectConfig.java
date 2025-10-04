5cd0b06c0137aef227744e5ba882bebd3f58a77d
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-10-04 08:55:35 +0700

    Listing 12.6 Defining a DataSource Bean for your project

--- src/main/java/spring/learning/spring_vault/config/ProjectConfig.java
+++ src/main/java/spring/learning/spring_vault/config/ProjectConfig.java
@@ -1,7 +1,30 @@
 package spring.learning.spring_vault.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectConfig {
+    @Value("${custom.datasource.url}")
+    private String dataSourceUrl;
+
+    @Value("${custom.datasource.password}")
+    private String dataSourcePassword;
+
+    @Value("${custom.datasource.username}")
+    private String dataSourceUserName;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(dataSourceUrl);
+        dataSource.setUsername(dataSourceUserName);
+        dataSource.setPassword(dataSourcePassword);
+        dataSource.setConnectionTimeout(30000);
+        return dataSource;
+    }
 }
